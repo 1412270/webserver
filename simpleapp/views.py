77db4e5c40001af453d918b2872cf4e5d1987c48
kiet-lta <ltakiet96@gmail.com>
2023-10-20 22:43:07 +0700

    feat(user_data): fetch data from Express api via the Python proxy

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+proxies = {
+   'http': 'http://localhost:8000',
+   'https': 'https://localhost:8000'
+}
+
+
+# Make a request to the proxy server running on localhost:8080.
+# The proxy server will forward the request to the Express API server
+def user_view(request):
+    url = 'http://localhost:3000/users'
+    response = requests.get(url, proxies=proxies)
+    user_data = response.json()
+    return JsonResponse(user_data, safe=False)
+
